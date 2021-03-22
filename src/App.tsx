fa0c34a1b91e0f92e03a67226359b737193c1a63
Alexander Avaliani <alexandr_1302@mail.ru>
2021-03-22 23:14:59 +0300

    - set apollo-client and graphql-gen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import RouterComponent from './RouterComponent';
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo-config';
 
 function App() {
-  return <RouterComponent />;
+  return (
+    <ApolloProvider client={client}>
+      <RouterComponent />
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- src/pages/feed/Feed.tsx
+++ src/pages/feed/Feed.tsx
@@ -5,11 +5,15 @@ import { Album, AlbumSkeleton, AlbumCreate } from '../../common/Album';
 import { Post, PostSkeleton } from '../../common/Post';
 import styles from './Feed.module.scss';
 import { ReactComponent as AcuraAvatar } from '../../assets/icons/acuraAvatar.svg';
+import { useGetAlbumsQuery } from '../../generated/graphql';
 
 type Props = unknown;
 
 const Feed: React.FC<Props> = () => {
   const [selected, setSelected] = React.useState<string>('Albums');
+
+  const { data, loading, error } = useGetAlbumsQuery();
+
   const tabs = [
     { label: 'Albums', count: 3 },
     { label: 'Posts', count: 1 },
@@ -21,13 +25,13 @@ const Feed: React.FC<Props> = () => {
       <div className={styles.content}>
         <div className={styles.title}>Feed</div>
         <TabNav tabs={tabs} selected={selected} setSelected={setSelected} />
-        <TabPanel isSelected={selected === 'Albums'}>
+        <TabPanel isSelected={selected === tabs[0].label}>
           <AlbumCreate />
           <Album logo={<AcuraAvatar />} name="Ivan Ivanov" title="Acura Summer 2018" />
           <AlbumSkeleton />
           <Album logo={<AcuraAvatar />} name="Ivan Ivanov" title="Acura Summer 2018" />
         </TabPanel>
-        <TabPanel isSelected={selected === 'Posts'}>
+        <TabPanel isSelected={selected === tabs[1].label}>
           <Post
             name="Ivan Ivanov"
             title="Title"
